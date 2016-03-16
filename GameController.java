74e45c5782665cf8982257df2bf8dcf6bf82c4ac
trysonl <tdlindle@uark.edu>
2016-03-16 17:14:33 -0500

    Added files via upload

--- GameController.java
+++ GameController.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class GameController {
 	public Masterboard mboard = new Masterboard();
 	public int boardNumber = 9;
+	private int Player;
 	
 	private void initialize()
 	{
@@ -16,13 +17,18 @@ public class GameController {
 		mboard.printBoard();
 	}
 	
-	private void firstTurn()
+	/*private void choiceTurn()
 	{
-		int firstNumber = Integer.parseInt(JOptionPane.showInputDialog("Pick the number of the board you want to play inside.\n"));
-		int location = Integer.parseInt(JOptionPane.showInputDialog("Pick the number of the spot you want to play inside that board.\n"));
+		int firstNumber = Integer.parseInt(JOptionPane.showInputDialog("Player " + Player + ": Pick the number of the board you want to play inside.\n"));
+		if (mboard.sboards[firstNumber - 1].checkWin())
+		{
+			choiceTurn();
+		}
+		int location = Integer.parseInt(JOptionPane.showInputDialog("Player " + Player + ": Pick the number of the spot you want to play inside that board.\n"));
 		mboard.sboards[firstNumber - 1].setContent('X', location - 1);
 		boardNumber = location - 1;
 	}
+	*/
 	
 	private void turn(char letter, int location)
 	{
@@ -30,35 +36,40 @@ public class GameController {
 		boardNumber = location - 1;
 	}
 	
-	private void playerTurn(int Player)
-	{
-		int location;
-		if (Player == 1)
-		{
-			location = Integer.parseInt(JOptionPane.showInputDialog("Player 1: Pick the number of the spot you want to play inside board number " + (boardNumber + 1) + ".\n"));
-			turn('X', location);
-		}
-		else if (Player == 2)
-		{
-			location = Integer.parseInt(JOptionPane.showInputDialog("Player 2: Pick the number of the spot you want to play inside board number " + (boardNumber + 1) + ".\n"));
-			turn('O', location);
-		}
-	}
+//	private void playerTurn()
+//	{
+//		int location;
+//		if (mboard.sboards[boardNumber].checkWin())
+//		{
+//			choiceTurn();
+//			
+//		}
+//		if (Player == 1)
+//		{
+//			location = Integer.parseInt(JOptionPane.showInputDialog("Player 1: Pick the number of the spot you want to play inside board number " + (boardNumber + 1) + ".\n"));
+//			turn('X', location);
+//		}
+//		else if (Player == 2)
+//		{
+//			location = Integer.parseInt(JOptionPane.showInputDialog("Player 2: Pick the number of the spot you want to play inside board number " + (boardNumber + 1) + ".\n"));
+//			turn('O', location);
+//		}
+//	}
 	
 	public void run()
 	{
 		initialize();
 		printGame();
-		int Player = 1;
-		firstTurn();
+		Player = 1;
+		choiceTurn();
 		printGame();
 		Player++;
 		while(true)
 		{
-			playerTurn(Player);
+			playerTurn();
 			printGame();
 			Player--;
-			playerTurn(Player);
+			playerTurn();
 			printGame();
 			Player++;	
 		}
--- SmallBoard.java
+++ SmallBoard.java
@@ -15,6 +15,86 @@ public class SmallBoard {
 		}
 	}
 	
+	public boolean checkEmpty(int location)
+	{
+		boolean isEmpty = false;
+		if (boardContents[location] == 'Z')
+			isEmpty = true;
+		return isEmpty;
+	}
+	
+	public boolean checkWin()
+	{
+		boolean didWin = false;
+		if (checkRow() || checkColumn() || checkDiag())
+			didWin = true;
+		return didWin;
+			
+	}
+	
+	public boolean checkRow()
+	{
+		boolean didWin = false;
+		if(boardContents[0] != 'Z')
+		{
+			if (boardContents[0] == boardContents[1] && boardContents[1] == boardContents[2])
+				didWin = true;
+		}
+		
+		if(boardContents[3] != 'Z')
+		{
+			if (boardContents[3] == boardContents[4] && boardContents[4] == boardContents[5])
+				didWin = true;
+		}
+		
+		if(boardContents[6] != 'Z')
+		{
+			if (boardContents[6] == boardContents[7] && boardContents[7] == boardContents[8])
+				didWin = true;
+		}
+		return didWin;
+	}
+	
+	public boolean checkColumn()
+	{
+		boolean didWin = false;
+		if(boardContents[0] != 'Z')
+		{
+			if (boardContents[0] == boardContents[3] && boardContents[3] == boardContents[6])
+				didWin = true;
+		}
+		
+		if(boardContents[1] != 'Z')
+		{
+			if (boardContents[1] == boardContents[4] && boardContents[4] == boardContents[7])
+				didWin = true;
+		}
+		
+		if(boardContents[2] != 'Z')
+		{
+			if (boardContents[2] == boardContents[5] && boardContents[5] == boardContents[8])
+				didWin = true;
+		}
+		return didWin;
+	}
+	
+	public boolean checkDiag()
+	{
+		boolean didWin = false;
+		if(boardContents[0] != 'Z')
+		{
+			if (boardContents[0] == boardContents[4] && boardContents[4] == boardContents[8])
+				didWin = true;
+		}
+		
+		if(boardContents[2] != 'Z')
+		{
+			if (boardContents[2] == boardContents[4] && boardContents[4] == boardContents[6])
+				didWin = true;
+		}
+		return didWin;
+	}
+	
 	public void setContent(char letter, int location)
 	{
 		boardContents[location] = letter;
