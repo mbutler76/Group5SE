ea1e1a4eb66d164a0535e0ddd05653c5a6720f8c
mikamoore <mikamoore93@gmail.com>
2016-04-26 11:14:54 -0500

    Added files via upload

--- Client.java
+++ Client.java
@@ -357,7 +357,7 @@ public class Client
         int num = 0;
         while (true)
         {
-            String serverAddress = "192.168.2.2";
+            String serverAddress = "72.221.62.219";
             Client client = new Client(serverAddress);
             client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             client.frame.setSize(600, 400);
